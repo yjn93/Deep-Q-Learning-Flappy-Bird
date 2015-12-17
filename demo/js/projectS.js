ce8cc2df7acbb4795749fcde7ff84621c01fc822
Yulin <yulin.shi.app@gmail.com>
2015-12-16 23:01:52 -0500

    conclusion

--- demo/js/projectS.js
+++ demo/js/projectS.js
@@ -73,9 +73,8 @@ var canvas, ctx;
     // Pipe is made up with two pipes and a gap between.
     var Pipe = function(x, height) {
       this.gap = new Vec(x, height); // position of the gap
-      this.size = 100 //default size of pipe
-      this.v = -1; // default moving velocity
-      this.cleanup_ = false;
+      this.size = 80 //default size of pipe
+      this.v = -2; // default moving velocity
     }
     
     var World = function() {
@@ -84,8 +83,6 @@ var canvas, ctx;
       this.H = canvas.height;
       
       this.clock = 0;
-
-
       this.walls = []; 
       this.pad = 5;
       this.walls.push(new Wall(new Vec(0, this.pad), new Vec(this.W, this.pad)));
@@ -93,9 +90,9 @@ var canvas, ctx;
 
       // set up pipes pool
       this.pipes = [];
-      for(var k=1;k<7;k++) {
-        var x = k*400
-        var y = 50*convnetjs.randi(2, 9); // the gap's height can be integer 1 to 7
+      for(var k=1;k<6;k++) {
+        var x = k*300 - 100;
+        var y = 25*convnetjs.randi(5, 15); // the gap's height can be integer 2 to 8
         var it = new Pipe(x, y);
         this.pipes.push(it);
       }
@@ -106,10 +103,13 @@ var canvas, ctx;
       stuff_collide_: function(closest_pipe) {
         var collide = false;
         var bird = this.agent;
+        var bird_x = bird.position.x;
+        var bird_y = bird.position.y;
         //collide with walls
         
-        if(bird.position.y<this.pad || bird.position.y>this.H-this.pad){
-          return true;
+        if(bird_y<this.pad || bird_y>this.H-this.pad){
+          //console.log("collision with walls");
+          return 1;
         }
           
         for(var i=0;i<this.walls.length;i++) {
@@ -117,115 +117,90 @@ var canvas, ctx;
           collide = line_point_intersect(wall.p1, wall.p2, bird.position, bird.rad);
         }
         if(collide) {
+          //console.log("collision with walls");
           return 1; //if collide with wall, return collision type 1
         }
         var frames = [];
         var pipe_x = closest_pipe.gap.x;
         var gap_h = closest_pipe.gap.y;
-        var gap_size = this.H/5;
-        util_add_box(frames, pipe_x, 0, closest_pipe.size, gap_h-gap_size);
-        util_add_box(frames, pipe_x, gap_h+gap_size, closest_pipe.size, this.H-gap_h+gap_size);        
-        for(var i=0;i<frames.length;i++) {
-          var frame = frames[i];
-          collide = line_point_intersect(frame.p1, frame.p2, bird.position, bird.rad);   
-          if(collide) {
-            return Math.abs(gap_h - bird.position.y)/this.H; //if collide with pipe, return destance to gap
-          }        
-        }
-        ctx.fillStyle = "rgb(0, 255, 0)";
-        ctx.rect(0,0,40,40);
-        ctx.fill();      
-        return false;
-        
-      },
-      update_world: function() {
-        this.agent.velocity += this.agent.gravity;
-        this.agent.position.y += this.agent.velocity;
-        // update all pipes
-        for(var i=0;i<this.pipes.length;i++) {
-          var pipe = this.pipes[i];
-          pipe.gap.x += pipe.v;
-        }
-        if(this.pipes[0].gap.x<-100){
-          this.pipes.shift();
-          var x = this.pipes.length*400+100;
-          var y = 50*convnetjs.randi(2, 9); // the gap's height can be integer 1 to 7
-          var it = new Pipe(x, y);
-          this.pipes.push(it);
-        }
-
+        var gap_size = 75;
+        if(bird_x<pipe_x-bird.rad)
+          return false;
+        else if (bird_y>(gap_h-gap_size+bird.rad)&&bird_y<(gap_h+gap_size-bird.rad)) 
+          return false;
+        else
+          return Math.abs(gap_h - bird.position.y)/this.H;
       },
       tick: function() {
         // tick the environment
         this.clock++;
-        if(this.clock) {
-          var current_pipe;
-          for(var i=0;i<this.pipes.length;i++) {
-            var pipe = this.pipes[i];
-            if(pipe.gap.x>0&&pipe.gap.x<=400){
-              current_pipe = pipe;
-            }
+
+        var current_pipe;
+        for(var i=0;i<this.pipes.length;i++) {
+          var pipe = this.pipes[i];
+          if(pipe.gap.x>0&&pipe.gap.x<=300){
+            current_pipe = pipe;
           }
-          this.agent.current_gap = current_pipe.gap;
-          // let the agent behave in the world based on its input
-          this.agent.forward();
-          
-          
-          // apply outputs of agent on evironment
-          if(this.agent.action) {
-            //action type 1, flap
-            ctx.fillStyle = "rgb(0, 255, 0)";
-            ctx.rect(0,0,40,40);
-            ctx.fill();
-            this.agent.velocity = -5; //update velocity of the bird to +5
-            this.agent.position.y += this.agent.velocity;
-          }
-          else {
-            //do not flap
-            this.agent.velocity += this.agent.gravity;
-            this.agent.position.y += this.agent.velocity;
-          }
-          
-          // tick all pipes
-          for(var i=0;i<this.pipes.length;i++) {
-            var pipe = this.pipes[i];
-            pipe.gap.x += pipe.v;
-          }
-          if(this.pipes[0].gap.x<-100){
-            this.pipes.shift();
-            var x = this.pipes.length*400+100;
-            var y = 50*convnetjs.randi(2, 9); // the gap's height can be integer 1 to 7
+        }
+        this.agent.current_gap = current_pipe.gap;
+        // let the agent behave in the world based on its input
+        this.agent.forward();
+        
+        
+        // apply outputs of agent on evironment
+        if(this.agent.action) {
+          //action type 1, flap
+          this.agent.velocity = -5; //update velocity of the bird to -5
+          this.agent.position.y += this.agent.velocity;
+        }
+        else {
+          //do not flap
+          this.agent.velocity += this.agent.gravity;
+          this.agent.position.y += this.agent.velocity;
+        }
+        
+        // tick all pipes
+        for(var i=0;i<this.pipes.length;i++) {
+          var pipe = this.pipes[i];
+          pipe.gap.x += pipe.v;
+        }
+        if(this.pipes[0].gap.x<=-100){
+          this.pipes.shift();
+          var x = this.pipes.length*300+200;
+          var y = 25*convnetjs.randi(5, 15); // the gap's height can be integer 2 to 8
+          var it = new Pipe(x, y);
+          this.pipes.push(it);
+        }
+
+        var bird_x = this.agent.position.x;
+        var bird_y = this.agent.position.y;
+
+
+        var collision = this.stuff_collide_(current_pipe);
+
+        if(collision) {
+          // bird collide with wall or pipe
+          this.agent.collision_sense = collision;
+          this.agent.reload();
+          // reset pipes pool
+          this.pipes = [];
+          for(var k=1;k<7;k++) {
+            var x = k*300 - 100;
+            var y = 25*convnetjs.randi(5, 15); // the gap's height can be integer 1 to 7
             var it = new Pipe(x, y);
             this.pipes.push(it);
           }
-
-          var bird_x = this.agent.position.x;
-          var bird_y = this.agent.position.y;
-
-
-          var collision = this.stuff_collide_(current_pipe);
-          if(collision) {
-            // bird collide with wall or pipe
-            this.collision_sense = collision;
-            this.agent.reload();
-            // reset pipes pool
-            this.pipes = [];
-            for(var k=1;k<7;k++) {
-              var x = k*400
-              var y = convnetjs.randi(2, 9); // the gap's height can be integer 1 to 7
-              var it = new Pipe(x, y*50);
-              this.pipes.push(it);
-            }
-          }
-          else {
-            this.collision_sense = 0;
-          }
-          // agents are given the opportunity to learn based on feedback of their action on environment
-          this.agent.backward();
-        } 
-        else {
-          this.update_world();
         }
+        else {
+          this.agent.collision_sense = 0;
+          this.agent.score ++;
+
+          if(this.agent.score/150>this.agent.best_score)
+            this.agent.best_score = this.agent.score/150;
+         }
+        // agents are given the opportunity to learn based on feedback of their action on environment
+        this.agent.backward();
+        
       }
 
     }
@@ -237,12 +212,14 @@ var canvas, ctx;
       this.position = new Vec(100, 250);
       
       // properties
-      this.rad = 10; //default radius
+      this.rad = 15; //default radius
       this.velocity = 0;
       this.gravity = 0.5; //default gravity
       this.current_gap = new Vec(200,250);
       //reward signal
       this.collision_sense = 0;
+      this.score = 60;
+      this.best_score = 0;
 
       // brain
       //this.brain = new deepqlearn.Brain(this.eyes.length * 3, this.actions.length);
@@ -260,24 +237,31 @@ var canvas, ctx;
         // in forward pass the agent simply behaves in the environment
         // create input to brain
         var input_array = new Array(3);
-        input_array[0] = this.velocity; //normalize velocity
-        input_array[1] = this.current_gap.x/200;
-        input_array[2] = (this.current_gap.y-100)/800;
+        max_velocity = Math.sqrt(2*this.gravity*canvas.height);
+        input_array[0] = (this.velocity+5)/25; //normalize velocity
+        input_array[1] = this.position.y/canvas.height;
+        input_array[2] = this.current_gap.x/300;
+        input_array[3] = (this.current_gap.y-100)/300;
         
         // get action from brain
         this.action = this.brain.forward(input_array);
+        //console.log("action:",this.action, this.velocity);
       },
       backward: function() {
         // in backward pass agent learns.
         // compute reward 
         var reward = 0.0;
         if(this.collision_sense){
-          reward = -1000*this.collision_sense;
+          reward = (-10)*this.collision_sense;
         }
         else{
-          reward = 1;
+          if(this.current_gap.x<100)
+            reward = 1;
+          else
+            reward = 1 - 0.5*Math.abs(this.current_gap.y-this.position.y)/500;
         }
-            
+
+        //console.log("reward",reward);
         // pass to brain for learning
         this.brain.backward(reward);
       },
@@ -286,50 +270,73 @@ var canvas, ctx;
         this.position = new Vec(100, 250);       
         this.actions = 0; //action can be 0 or 1, 0: do not flap; 1: flap
         // properties
-        this.rad = 10; //default radius
+        this.rad = 15; //default radius
         this.velocity = 0;
-        this.gravity = 0.5; //default gravity
+        this.gravity = 0.25; //default gravity
         this.current_gap = new Vec(200,250);
+        this.score = 60;
       }
     }
     
-    
+    var reward_graph = new cnnvis.Graph();
+    function draw_stats() {
+      var b = w.agent.brain;
+      if(w.clock % 500 === 0) {
+        reward_graph.add(w.clock/500, b.average_reward_window.get_average());
+        var gcanvas = document.getElementById("graph_canvas");
+        reward_graph.drawSelf(gcanvas);
+      }
+    }
+        
     // Draw everything
     function draw() {  
-      ctx.clearRect(0, 0, canvas.width, canvas.height);
-      ctx.lineWidth = 1;
-      var agent = w.agent;
-      
-      // draw walls in environment
-      ctx.strokeStyle = "rgb(0,0,0)";
-      ctx.beginPath();
-      for(var i=0,n=w.walls.length;i<n;i++) {
-        var q = w.walls[i];
-        ctx.moveTo(q.p1.x, q.p1.y);
-        ctx.lineTo(q.p2.x, q.p2.y);
-      }
-      ctx.stroke();
-  
-      // draw agents
-      ctx.fillStyle = "rgb(255,0,0)";
-      ctx.strokeStyle = "rgb(0,0,0)";
-      ctx.beginPath();
-      ctx.arc(agent.position.x, agent.position.y, agent.rad, 0, Math.PI*2, true); 
-      ctx.fill();
-      ctx.stroke();
-      
-      // draw pipes
-      ctx.strokeStyle = "rgb(0,0,0)";
-      ctx.fillStyle = "rgb(0, 255, 0)";
-      for(var i=0,n=w.pipes.length;i<n;i++) {
-        var pipe = w.pipes[i];    
-        ctx.beginPath();
-        ctx.rect(pipe.gap.x,0,pipe.size,pipe.gap.y-w.H/5);
-        ctx.rect(pipe.gap.x,pipe.gap.y+w.H/5,pipe.size,w.H-pipe.gap.y+w.H/5);
-        ctx.fill();
-        ctx.stroke();
-      }
-      
+      // ctx.clearRect(0, 0, canvas.width, canvas.height);
+      // ctx.lineWidth = 1;
+      // var agent = w.agent;
+
+      // //draw background
+      // var sky = document.getElementById("background");
+      // ctx.drawImage(sky, 0, 0, 800, 500);
+
+
+      // // draw walls in environment
+      // ctx.strokeStyle = "rgb(0,0,0)";
+      // ctx.beginPath();
+      // for(var i=0,n=w.walls.length;i<n;i++) {
+      //   var q = w.walls[i];
+      //   ctx.moveTo(q.p1.x, q.p1.y);
+      //   ctx.lineTo(q.p2.x, q.p2.y);
+      // }
+      // ctx.stroke();
+
+      // var bird_img = document.getElementById("bird");
+      // ctx.drawImage(bird_img, agent.position.x-20, agent.position.y-15, 38, 30); 
+
+      // // draw pipes      
+      // ctx.strokeStyle = "rgb(0,0,0)";     
+      // for(var i=0,n=w.pipes.length;i<n;i++) {
+      //   var pipe = w.pipes[i]; 
+      //   var grd = ctx.createLinearGradient(pipe.gap.x, 0, pipe.gap.x+pipe.size, 0);  
+      //   ctx.fillStyle = grd; 
+      //   ctx.beginPath();
+      //   grd.addColorStop(0, 'rgb(150,180,80)');
+      //   grd.addColorStop(0.3, 'rgb(255,250,150)');
+      //   grd.addColorStop(1, 'rgb(150,180,90)');
+
+      //   ctx.rect(pipe.gap.x+2,5,pipe.size-4,pipe.gap.y-75-5-20);
+      //   ctx.rect(pipe.gap.x,pipe.gap.y-75-20,pipe.size,20);
+      //   ctx.rect(pipe.gap.x,pipe.gap.y+75,pipe.size,20);
+      //   ctx.rect(pipe.gap.x+2,pipe.gap.y+75+20,pipe.size-4,w.H-pipe.gap.y-75-5-20);
+       
+      //   ctx.fill();
+      //   ctx.stroke();
+      // }
+
+      // ctx.fillStyle = "rgb(0, 0, 0)";
+      // ctx.font = "30px Arial";
+      // ctx.fillText((Math.floor(agent.score/150)).toString(),10,50);
+      // ctx.font = "20px Arial";
+      // ctx.fillText("Best_score: "+(Math.floor(agent.best_score)).toString(),10,480);     
       w.agent.brain.visSelf(document.getElementById('brain_info_div'));
     }
     
@@ -337,17 +344,44 @@ var canvas, ctx;
     function tick() {
       w.tick();
       draw();
+      draw_stats();
       
     }
     
     var simspeed = 2;
 
+    function gofast() {
+      window.clearInterval(current_interval_id);
+      current_interval_id = setInterval(tick, 2);
+      skipdraw = false;
+      simspeed = 2;
+    }
+
     function gonormal() {
       window.clearInterval(current_interval_id);
-      current_interval_id = setInterval(tick, 10);
+      current_interval_id = setInterval(tick, 20);
       skipdraw = false;
       simspeed = 1;
     }
+
+    function savenet() {
+      var j = w.agent.brain.value_net.toJSON();
+      var t = JSON.stringify(j);
+      document.getElementById('updatenet').value = t;
+    }
+    
+    function loadnet() {
+      var t = document.getElementById('updatenet').value;
+      var j = JSON.parse(t);
+      w.agent.brain.value_net.fromJSON(j);
+      stoplearn(); // also stop learning
+      gonormal();
+    }
+
+    function stoplearn() {
+      w.agent.brain.learning = false;
+    }   
+
     var w; // global world object
     var current_interval_id;
     var skipdraw = false;
@@ -355,5 +389,5 @@ var canvas, ctx;
       canvas = document.getElementById("canvas");
       ctx = canvas.getContext("2d");    
       w = new World();    
-      gonormal();
+      gofast();
     }
